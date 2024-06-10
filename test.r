52b0e487415cbc9ab1ca7734330e9886347591b0
iSlowz <mbellenger118@gmail.com>
2024-06-10 14:04:48 +0200

    ez

--- test.r
+++ test.r
@@ -28,10 +28,10 @@ utf8 <- function(data) {
     return(data)
 }
 
-data_utf8 = utf8(data)
-print(head(data_utf8))
-View(data_utf8)
-
+data = utf8(data)
+# print(head(data))
+# View(data)
+#aaaa
 
 data$X <- as.numeric(data$X) #a converti la colonne X en numérique
 data$Y <- as.numeric(data$Y) #a converti la colonne Y en numérique
@@ -53,11 +53,30 @@ data$fk_situation <- as.character(data$fk_situation) #a converti la colonne fk_s
 data$fk_revetement <- as.character(data$fk_revetement) #a converti la colonne fk_revetement en caractère
 data$commentaire_environnement <- as.character(data$commentaire_environnement) #a converti la colonne commentaire_environnement en caractère
 data$dte_plantation <- as.Date(data$dte_plantation, format = "%Y-%m-%d %H:%M:%S%z") #a converti la colonne dte_plantation en date
-date$age_estim <- as.numeric(data$age_estim) #a converti la colonne age_estim en numérique
-date$fk_prec_estim <- as.numeric(data$fk_prec_estim) #a converti la colonne fk_prec_estim en numérique
-date$clc_nbr_diag <- as.numeric(data$clc_nbr_diag) #a converti la colonne clc_nbr_diag en numérique
+data$age_estim <- as.numeric(data$age_estim) #a converti la colonne age_estim en numérique
+data$fk_prec_estim <- as.numeric(data$fk_prec_estim) #a converti la colonne fk_prec_estim en numérique
+data$clc_nbr_diag <- as.numeric(data$clc_nbr_diag) #a converti la colonne clc_nbr_diag en numérique
+data$dte_abattage <- as.Date(data$dte_abattage, format = "%Y-%m-%d %H:%M:%S%z") #a converti la colonne dte_abattage en date
+data$fk_nomtech <- as.character(data$fk_nomtech) #a converti la colonne fk_nomtech en caractère
+data$last_edited_user <- as.character(data$last_edited_user) #a converti la colonne last_edited_user en caractère
+data$last_edited_date <- as.Date(data$last_edited_date, format = "%Y-%m-%d %H:%M:%S%z") #a converti la colonne last_edited_date en date
+data$villeca <- as.character(data$villeca) #a converti la colonne villeca en caractère
+data$nomfrancais <- as.character(data$nomfrancais) #a converti la colonne nomfrancais en caractère
+data$nomlatin <- as.character(data$nomlatin) #a converti la colonne nomlatin en caractère
+data$GlobalID <- as.character(data$GlobalID) #a converti la colonne GlobalID en caractère  
+data$CreationDate <- as.Date(data$CreationDate, format = "%Y-%m-%d %H:%M:%S%z") #a converti la colonne CreationDate en date
+data$Creator <- as.character(data$Creator) #a converti la colonne Creator en caractère
+data$EditDate <- as.Date(data$EditDate, format = "%Y-%m-%d %H:%M:%S%z") #a converti la colonne EditDate en date
+data$Editor <- as.character(data$Editor) #a converti la colonne Editor en caractère
+data$feuillage <- as.character(data$feuillage) #a converti la colonne feuillage en caractère
+data$remarquable <- as.character(data$remarquable) #a converti la colonne remarquable en caractère
 
+#affiche type de data$X
+print(class(data$X))
 
+print(head(data))
+print(summary(data))
+View(data)
 
 
 # #Conversion des types de données
