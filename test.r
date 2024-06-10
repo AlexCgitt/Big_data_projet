a2601658527d64f35e85c95ac8d1fbc59c40942d
AlexCgitt <alexmicrosoft03@gmail.com>
2024-06-10 11:55:10 +0200

    egg

--- test.r
+++ test.r
@@ -13,18 +13,38 @@ TACHES A FAIRE :
 3. Visualisation des données sur une carte
 4. Etude des corrélations
 5. Prédiction de la variable « Age estimé »
-
+6. Régression
+7. Export pour l'IA
+8. Livrable
 "
 
-# Importation des données
-# Read the CSV file "Patrimoine_Arbore.csv" into the 'data' variable
-# Parameters:
-#   - header: TRUE to indicate that the CSV file has a header row
-#   - encoding: "UTF-8" to specify the character encoding of the file
-#   - sep: "," to specify the separator used in the CSV file
-# test
+"
+---------------------------------------------------------------------------------
+1. Exploration donné
+	- Description du jeu de données
+	- Conversion des types de données (des caractère en numéric)
+	- Corriger l'encodage de UTF-8
+	- Nettoyer les données (valeurs manquantes, valeurs aberrantes, doublons)
+	- Analyse exploratoires (les graphes)
+"
+
+
+"
+Importation des données
+    - Read the CSV file Patrimoine_Arbore.csv into the 'data' variable
+    - header: TRUE to indicate that the CSV file has a header row
+    - sep: , to specify the separator used in the CSV file
+"
 data <- read.csv("Patrimoine_Arbore.csv", header = TRUE, sep = ",")
 
+
+"
+Corriger l'encodage de UTF-8
+    - fonction utf8
+        - Convertir les données de latin à UTF-8
+        - Utiliser la fonction iconv pour convertir les données
+        - Boucle sur chaque colonne de data et convertir les données de latin à UTF-8
+"
 utf8 <- function(data) {
     # boucle sur chaque colonnes de data et convertit les données de latin à UTF-8
     for (col in colnames(data)) {
