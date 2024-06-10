818afb20acfa9d5a68bbc1e824d0af514d61ba86
AlexCgitt <alexmicrosoft03@gmail.com>
2024-06-10 14:31:45 +0200

    yasss

--- test.r
+++ test.r
@@ -17,8 +17,8 @@ stp
 ---------------------------------------------------------------------------------
 1. Exploration donné
 	- Description du jeu de données 
-	- Conversion des types de données (des caractère en numéric) X
-	- Corriger l'encodage de UTF-8 X
+	- Conversion des types de données (des caractère en numéric) 
+	- Corriger l'encodage de UTF-8
 	- Nettoyer les données (valeurs manquantes, valeurs aberrantes, doublons)
 	- Analyse exploratoires (les graphes)
 "
@@ -57,6 +57,12 @@ data = utf8(data)
 Description du jeu de données   
 "
 
+"
+Conversion des types de données (des caractère en numéric)
+utilisation de la fonction as.numeric pour convertir les données de caractère en numérique
+utilisation de la fonction as.Date pour convertir les données de caractère en date
+utilisation de la fonction as.character pour convertir les données de numérique en caractère
+"
 data$X <- as.numeric(data$X) #a converti la colonne X en numérique
 data$Y <- as.numeric(data$Y) #a converti la colonne Y en numérique
 data$OBJECTID <- as.numeric(data$OBJECTID) #a converti la colonne OBJECTID en numérique
@@ -76,38 +82,114 @@ data$fk_pied <- as.character(data$fk_pied) #a converti la colonne fk_pied en car
 data$fk_situation <- as.character(data$fk_situation) #a converti la colonne fk_situation en caractère
 data$fk_revetement <- as.character(data$fk_revetement) #a converti la colonne fk_revetement en caractère
 data$commentaire_environnement <- as.character(data$commentaire_environnement) #a converti la colonne commentaire_environnement en caractère
-data$dte_plantation <- as.Date(data$dte_plantation, format = "%Y-%m-%d %H:%M:%S%z") #a converti la colonne dte_plantation en date
+data$dte_plantation <- as.Date(data$dte_plantation, format = "%Y-%m-%d %H:%M:%S+%z") #a converti la colonne dte_plantation en date
 data$age_estim <- as.numeric(data$age_estim) #a converti la colonne age_estim en numérique
 data$fk_prec_estim <- as.numeric(data$fk_prec_estim) #a converti la colonne fk_prec_estim en numérique
 data$clc_nbr_diag <- as.numeric(data$clc_nbr_diag) #a converti la colonne clc_nbr_diag en numérique
 data$dte_abattage <- as.Date(data$dte_abattage, format = "%Y-%m-%d %H:%M:%S%z") #a converti la colonne dte_abattage en date
 data$fk_nomtech <- as.character(data$fk_nomtech) #a converti la colonne fk_nomtech en caractère
 data$last_edited_user <- as.character(data$last_edited_user) #a converti la colonne last_edited_user en caractère
-data$last_edited_date <- as.Date(data$last_edited_date, format = "%Y-%m-%d %H:%M:%S%z") #a converti la colonne last_edited_date en date
+data$last_edited_date <- as.Date(data$last_edited_date, format = "%Y-%m-%d %H:%M:%S+%z") #a converti la colonne last_edited_date en date
 data$villeca <- as.character(data$villeca) #a converti la colonne villeca en caractère
 data$nomfrancais <- as.character(data$nomfrancais) #a converti la colonne nomfrancais en caractère
 data$nomlatin <- as.character(data$nomlatin) #a converti la colonne nomlatin en caractère
 data$GlobalID <- as.character(data$GlobalID) #a converti la colonne GlobalID en caractère  
-data$CreationDate <- as.Date(data$CreationDate, format = "%Y-%m-%d %H:%M:%S%z") #a converti la colonne CreationDate en date
+data$CreationDate <- as.Date(data$CreationDate, format = "%Y-%m-%d %H:%M:%S+%z") #a converti la colonne CreationDate en date
 data$Creator <- as.character(data$Creator) #a converti la colonne Creator en caractère
-data$EditDate <- as.Date(data$EditDate, format = "%Y-%m-%d %H:%M:%S%z") #a converti la colonne EditDate en date
+data$EditDate <- as.Date(data$EditDate, format = "%Y-%m-%d %H:%M:%S+%z") #a converti la colonne EditDate en date
 data$Editor <- as.character(data$Editor) #a converti la colonne Editor en caractère
 data$feuillage <- as.character(data$feuillage) #a converti la colonne feuillage en caractère
 data$remarquable <- as.character(data$remarquable) #a converti la colonne remarquable en caractère
 
 #affiche type de data$X
+#permet de verifier si la conversion a bien été faite
+print("data$X")
 print(class(data$X))
+print("data$Y")
+print(class(data$Y))
+print("data$OBJECTID")
+print(class(data$OBJECTID))
+print("data$created_date")
+print(class(data$created_date))
+print("data$created_user")
+print(class(data$created_user))
+print("data$src_geo")
+print(class(data$src_geo))
+print("data$clc_quartier")
+print(class(data$clc_quartier))
+print("data$clc_secteur")
+print(class(data$clc_secteur))
+print("data$id_arbre")
+print(class(data$id_arbre))
+print("data$haut_tot")
+print(class(data$haut_tot))
+print("data$haut_tronc")
+print(class(data$haut_tronc))
+print("data$tronc_diam")
+print(class(data$tronc_diam))
+print("data$fk_arb_etat")
+print(class(data$fk_arb_etat))
+print("data$fk_stadedev")
+print(class(data$fk_stadedev))
+print("data$fk_port")
+print(class(data$fk_port))
+print("data$fk_pied")
+print(class(data$fk_pied))
+print("data$fk_situation")
+print(class(data$fk_situation))
+print("data$fk_revetement")
+print(class(data$fk_revetement))
+print("data$commentaire_environnement")
+print(class(data$commentaire_environnement))
+print("data$dte_plantation")
+print(class(data$dte_plantation))
+print("data$age_estim")
+print(class(data$age_estim))
+print("data$fk_prec_estim")
+print(class(data$fk_prec_estim))
+print("data$clc_nbr_diag")
+print(class(data$clc_nbr_diag))
+print("data$dte_abattage")
+print(class(data$dte_abattage))
+print("data$fk_nomtech")
+print(class(data$fk_nomtech))
+print("data$last_edited_user")
+print(class(data$last_edited_user))
+print("data$last_edited_date")
+print(class(data$last_edited_date))
+print("data$villeca")
+print(class(data$villeca))
+print("data$nomfrancais")
+print(class(data$nomfrancais))
+print("data$nomlatin")
+print(class(data$nomlatin))
+print("data$GlobalID")
+print(class(data$GlobalID))
+print("data$CreationDate")
+print(class(data$CreationDate))
+print("data$Creator")
+print(class(data$Creator))
+print("data$EditDate")
+print(class(data$EditDate))
+print("data$Editor")
+print(class(data$Editor))
+print("data$feuillage")
+print(class(data$feuillage))
+print("data$remarquable")
+print(class(data$remarquable))
 
-print(head(data))
-print(summary(data))
-View(data)
+# print(head(data))
+# print(summary(data))
+# View(data)
 
 
-# #Conversion des types de données
-# print(head(data$EditDate)) #a affiché la structure de data
-# data$EditDate <- as.Date(data$EditDate, format = "%Y-%m-%d") #a converti la colonne EditDate en date
-# print(head(data$EditDate)) #a affiché la structure de data
+"
+Nettoyer les données
+    - Valeurs manquantes
+    - Valeurs aberrantes
+    - Doublons
+"
 
 na_indices <- which(is.na(data), arr.ind = TRUE)
 na_df <- data.frame(Ligne = na_indices[, 1], Colonne = colnames(data)[na_indices[, 2]])
-print(na_df)
+#print(na_df)
