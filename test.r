a1d403bd69e235922373e32bc4bf71c4aa0db30a
AlexCgitt <alexmicrosoft03@gmail.com>
2024-06-10 09:30:03 +0200

    1er comm

--- test.r
+++ test.r
@@ -1,2 +1,11 @@
 # Projet big data
-# maxime caca
+
+"
+Sujet : Concevoir et développer une application d’étude du patrimoine arboré
+
+
+Dans cette partie la on analyse et on traite les données
+
+1. Exploration donné
+
+"
